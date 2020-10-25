e209231b9c7e669410c6ac7e9bfe5dcc480ffe62
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-25 18:54:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         },
-				programmer_hash[:yukihiro_matsumoto]={:known_for=>"Ruby",:languages=>["LISP","C"]}
+				# programmer_hash[:yukihiro_matsumoto]={:known_for=>"Ruby",:languages=>["LISP","C"]}
      }
 
 	puts programmer_hash
